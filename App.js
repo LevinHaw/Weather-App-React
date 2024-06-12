5d1eae431c5f2e1a2821c18c514447c11ab6d2c6
Leevinkie H <hawtarilevin@gmail.com>
2024-06-12 16:26:12 +0700

    implement: add endpoint API and show it in weather info

--- App.js
+++ App.js
@@ -3,16 +3,31 @@ import { useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import WeatherSearch from "./src/components/weatherSearch";
 import WeatherInfo from "./src/components/weatherInfo";
+import axios from "axios";
+import { API_KEY, BASE_URL } from "./src/helpers/constants";
 
 const App = () => {
+  const [weatherData, setWeatherData] = useState();
   const searchWeather = (location) => {
-    console.log(location);
+    axios
+      .get(`${BASE_URL}?q=${location}&appId=${API_KEY}`)
+      .then((response) => {
+        const data = response.data;
+        data.visibility /= 100;
+        data.visibility = data.visibility.toFixed(2);
+        data.main.temp -= 273.15; // convert kelvin to cel
+        data.main.temp = data.main.temp.toFixed(2);
+        setWeatherData(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
     <View style={styles.container}>
       <WeatherSearch searchWeather={searchWeather} />
-      <WeatherInfo />
+      {weatherData && <WeatherInfo weatherData={weatherData} />}
     </View>
   );
 };
---
+++ src/helpers/constants.js
@@ -0,0 +1,3 @@
+export const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+export const API_KEY = "058f80e3479fda37603679d9987c9f40";
