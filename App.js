c9971188cb155ecb84619a0b3f6df096142f83d1
Leevinkie H <hawtarilevin@gmail.com>
2024-06-12 16:33:58 +0700

    add: function loading, success, and error

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
 import WeatherSearch from "./src/components/weatherSearch";
 import WeatherInfo from "./src/components/weatherInfo";
 import axios from "axios";
@@ -8,7 +8,28 @@ import { API_KEY, BASE_URL } from "./src/helpers/constants";
 
 const App = () => {
   const [weatherData, setWeatherData] = useState();
+
+  const [status, setStatus] = useState();
+
+  const renderComponent = () => {
+    switch (status) {
+      case "loading":
+        return <ActivityIndicator size="large" />;
+      case "success":
+        return <WeatherInfo weatherData={weatherData} />;
+      case "error":
+        return (
+          <Text>
+            Something went wrong. Please try again with a correct city name
+          </Text>
+        );
+      default:
+        return;
+    }
+  };
+
   const searchWeather = (location) => {
+    setStatus("loading");
     axios
       .get(`${BASE_URL}?q=${location}&appId=${API_KEY}`)
       .then((response) => {
@@ -18,16 +39,18 @@ const App = () => {
         data.main.temp -= 273.15; // convert kelvin to cel
         data.main.temp = data.main.temp.toFixed(2);
         setWeatherData(data);
+        setStatus("success");
       })
       .catch((error) => {
         console.log(error);
+        setStatus("error");
       });
   };
 
   return (
     <View style={styles.container}>
       <WeatherSearch searchWeather={searchWeather} />
-      {weatherData && <WeatherInfo weatherData={weatherData} />}
+      <View style={styles.marginTop20}>{renderComponent()}</View>
     </View>
   );
 };
@@ -35,6 +58,9 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     padding: 20,
+  },
+  marginTop20: {
+    marginTop: 20,
   },
 });
 
